fa84ca742fa2e275204ce2d7370b4b32e5f83b41
Sajib Das <120473050+Sajibcoder@users.noreply.github.com>
2023-03-06 19:15:52 +0530

    Update Projects.jsx

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -19,24 +19,32 @@ const Projects = () => {
       <img  src={image1} alt="" />
       <h1>Golden Store <br />(Ecommerce App)</h1>
       <a href="https://golden-store.netlify.app/"rel="noreferrer" target="_blank">View</a>
+      <a href="https://github.com/Sajibcoder/Golden-Store-E-Commerce-"rel="noreferrer" target="_blank">Source</a>
+
       </li>
 
      <li>
       <img src={image2} alt="" />
       <h1>Easy Recipe <br />(Recipe App)</h1>
       <a href="https://quality-recipe.netlify.app/" rel="noreferrer" target="_blank">View</a>
+      <a href="https://github.com/Sajibcoder/Easy-Recipe-App" rel="noreferrer" target="_blank">Source</a>
+
       </li>
 
     <li>
       <img src={image3} alt="" />
       <h1>Mental Care <br />(Service provider App)</h1>
       <a href="https://mental-care.netlify.app/" rel="noreferrer" target="_blank">View</a>
+      <a href="https://github.com/Sajibcoder/MENTAL-CARE-Service-Provider" rel="noreferrer" target="_blank">Source</a>
+
       </li>
 
       <li>
       <img src={image4} alt="" />
       <h1>People's News App <br />(Latest News Of India)</h1>
       <a href="https://peoplesnewsapp.netlify.app/" rel="noreferrer" target="_blank">View</a>
+      <a href="https://github.com/Sajibcoder/People-s-News-App" rel="noreferrer" target="_blank">Source</a>
+
       </li>
       
       </ul>
